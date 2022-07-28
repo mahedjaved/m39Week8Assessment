94827c864c553ce5d7262936d9ec4cf16d68f295
Mahed Javed <mahed95@gmail.com>
2022-07-28 15:59:06 +0100

    first of crud ops added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 // pull in yargs and sequelise item
 const yargs = require("yargs");
 const { sequelize } = require("./db/connection");
+const {
+	createMovie,
+	readMovie,
+	updateMovie,
+	deleteMovie,
+} = require("./movie/function");
+const Movie = require("./movie/table");
 
 // the main app with CRUD operations
 const app = async (yargsObj) => {
-	if (yargsObj.add) {
+	// get all of the tables I've created and set it up in MySQL db
+	await sequelize.sync({ alter: true });
+	if (yargsObj.create) {
 		// create movie from the database
+		await createMovie({ title: yargsObj.title, actor: yargsObj.actor });
 	} else if (yargsObj.read) {
 		// list movies from the database
+		await readMovie(yargsObj);
 	} else if (yargsObj.update) {
 		// update the movie datbase
+		await updateMovie(yargsObj);
 	} else if (yargsObj.delete) {
 		// delete the movie database
+		await deleteMovie(yargsObj);
 	} else {
 		console.log("Incorrect command");
 	}
--- src/movie/function.js
+++ src/movie/function.js
@@ -1,10 +1,71 @@
 // pull in the movie table
 const Movie = require("./table");
 
+// all the CRUD function declarations here
 exports.createMovie = async (movieObj) => {
+	// 'C' in CRUD
 	try {
 		// create the movie table --> like mongoose
 		const newMovie = await Movie.create(movieObj);
 		console.log(newMovie);
-	} catch {}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// 'R' in CRUD
+exports.readMovie = async (movieObj) => {
+	try {
+		// get the title searched for
+		const readMovie = await Movie.findAll({
+			where: { title: movieObj.findtitle },
+		});
+		const readActor = await Movie.findAll({
+			where: { actor: movieObj.findActor },
+		});
+		console.log(
+			`Are you looking for title ${readMovie} and the actor ${readActor}`
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// 'U' in CRUD
+exports.updateMovie = async (movieObj) => {
+	// 'U' in CRUD
+	try {
+		// get the title searched for
+		const updatedMovie = await Movie.update(
+			{ title: movieObj.newTitle },
+			{ actor: movieObj.newActor },
+			{
+				where: {
+					title: movieObj.title,
+					actor: movieObj.actor,
+				},
+			}
+		);
+
+		console.log(`Your updated movie is ${updatedMovie}`);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// 'D' in CRUD
+exports.deleteMovie = async (movieObj) => {
+	try {
+		// get the title searched for
+		const deletedMovie = await Movie.delete({
+			where: {
+				title: movieObj.title,
+				actor: movieObj.actor,
+			},
+		});
+
+		console.log(`You have discarded the title ${deletedMovie}`);
+	} catch (error) {
+		console.log(error);
+	}
 };
